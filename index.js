22a6ce1bc34af57c7ef0dee0f48c0d833b06d1a0
Nam Pham <hainamail1710@gmail.com>
2026-01-23 00:04:49 +1100

    Fix port for deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
